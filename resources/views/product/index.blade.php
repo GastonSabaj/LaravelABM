590bc24cc89551ddf5309557563c8652010f201a
Gaston <gsabaj@fi.uba.ar>
2024-03-02 15:35:58 -0300

    "Commit2 "

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -9,7 +9,7 @@
         <a href="{{ route('prueba') }}" type="button" class="btn btn-primary">Prueba</a>
         <i class="fa fa-spinner fa-spin fa-3x fa-fw"></i>
         <span class="sr-only">Loading...</span>
-        <a href="{{ route('products.create') }}" class="btn btn-primary">Add Product</a>
+        <a href="{{ route('products.create') }}" class="btn btn-primary">Add a Product</a>
     </div>
 
 
